a8378ebbe519b01835c57b74631cd91febd2d22e
Mwagiru <joymwagiru@gmail.com>
2022-04-07 08:40:38 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function(number1 , number2){
+    return 2000 / 100;
+}
+////////////////////
+const square = x => x * x;
+////////////////////////////
+const add = (num3, num4)=> num3 + num4;
